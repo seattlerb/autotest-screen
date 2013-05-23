e51a6e240e55e820f941e9abbb695b66a9ce8166
Ryan Davis <ryand@zenspider.com>
2013-05-22 21:57:46 -0800

    switched to minitest 5 [git-p4: depot-paths = "//src/autotest-screen/dev/": change = 8597]

--- test/test_autotest_screen.rb
+++ test/test_autotest_screen.rb
@@ -1,8 +1,8 @@
-require 'test/unit'
+require 'minitest/autorun'
 require 'rubygems'
 require 'autotest/screen'
 
-class TestAutotestScreen < Test::Unit::TestCase
+class TestAutotestScreen < Minitest::Test
   def test_all_added_hooks_called
     Autotest::HOOKS.keys.each do |hook|
       assert(Autotest::ALL_HOOKS.include?(hook), %Q!"#{hook}" never called.!)
